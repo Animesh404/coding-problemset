9422b7c6c8875c33994c894c0064426622863991
harshrana117 <52307926+harshrana117@users.noreply.github.com>
2020-10-01 10:14:44 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -15,3 +15,24 @@ int main()
         }
   
 }
+
+//harsh rana
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            cout<<a[i];
+        }
+  
+}
