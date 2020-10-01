7e6bcfeffd2d2112c8ed53e948635eab2a85d32a
PranjalKhatri <68675142+PranjalKhatri@users.noreply.github.com>
2020-10-01 07:22:02 +0530

    Update add.cpp
    Added comments

--- add.cpp
+++ add.cpp
@@ -2,12 +2,15 @@
 //write your name in comment like this and then your code below that comment
 
 //adarsh
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 int main()
 {
+  //Initialoze two numbers a and b of type int
   int a,b;
+  //Take values of a and b from user
   cin>>a>>b;
+  //Print their addition
   cout<<a+b;
   return 0;
 }
